7824e7c597fb02d1ecf1f45298348836fd8cb6ae
Rajangane <pratiksharajangane7@gmail.com>
2022-01-11 19:10:24 +0530

    Revert "[Pratiksha] ADD : Getting All Employee Details From Sql"
    This reverts commit 21194436b67c3ba7bf2a5d00a184fd2817df1d72.

--- EmployeePayRoll_ADO.Net/EmployeePayRoll_ADO.Net/Program.cs
+++ EmployeePayRoll_ADO.Net/EmployeePayRoll_ADO.Net/Program.cs
@@ -2,7 +2,7 @@
 //Console.WriteLine("*************** Welcome To Employee Payroll Service ********************");
 
 using EmployeePayRoll_ADO.Net;
-Console.Write("Select Number:\n1)GetAllEmployeeDetails");
+Console.Write("Select Number:\n1)GetAllEmployeeDetails\n2)AddEmployeeDetails");
 int option = Convert.ToInt32(Console.ReadLine());
 EmployeeModel employee = new EmployeeModel();
 EmployeeRepo repo = new EmployeeRepo();
