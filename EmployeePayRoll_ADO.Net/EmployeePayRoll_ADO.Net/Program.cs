324e5639d405cfc26f11dee1237e9295840a8300
Rajangane <pratiksharajangane7@gmail.com>
2022-01-11 19:38:02 +0530

    [Pratiksha] ADD : Adding Employee Details Using Stored Procedure

--- EmployeePayRoll_ADO.Net/EmployeePayRoll_ADO.Net/Program.cs
+++ EmployeePayRoll_ADO.Net/EmployeePayRoll_ADO.Net/Program.cs
@@ -2,7 +2,7 @@
 //Console.WriteLine("*************** Welcome To Employee Payroll Service ********************");
 
 using EmployeePayRoll_ADO.Net;
-Console.Write("Select Number:\n1)GetAllEmployeeDetails");
+Console.Write("Select Number:\n1)GetAllEmployeeDetails\n2)AddEmployeeDetails");
 int option = Convert.ToInt32(Console.ReadLine());
 EmployeeModel employee = new EmployeeModel();
 EmployeeRepo repo = new EmployeeRepo();
@@ -10,7 +10,23 @@ EmployeeRepo repo = new EmployeeRepo();
 switch (option)
 {
     case 1:
-       repo.GetAllEmployee();
+        repo.GetAllEmployee();
+        break;
+    case 2:
+        employee.EmployeeName = "Ruhi";
+        employee.PhoneNumber = "6363123485";
+        employee.Address = "Goa";
+        employee.Department = "IT";
+        employee.Gender = 'F';
+        employee.BasicPay = 10.000000;
+        employee.Deduction = 11.20;
+        employee.TaxablePay = 2000.525;
+        employee.Tax = 200.000;
+        employee.NetPay = 56.0000;
+        employee.City = "Goa";
+        employee.Country = "India";
+        repo.AddEmployee(employee);
+
         break;
     default:
         Console.WriteLine("\nInvalid Option");
